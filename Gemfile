0b590cef2595e76448177ca225f0b04ef56a747a
xiaofei <xiaofei@xiaofei-ubuntu.(none)>
2013-04-18 15:11:00 +0800

    Add a full_title helper

--- Gemfile
+++ Gemfile
@@ -5,17 +5,25 @@ gem 'rails', '3.2.13'
 # Bundle edge Rails instead:
 # gem 'rails', :git => 'git://github.com/rails/rails.git'
 
+gem 'pg','0.12.2'
+
 group :development,:test do
     gem 'sqlite3','1.3.5'
     gem 'rspec-rails','2.11.0'
+    gem 'guard-rspec','1.2.1'
+
+    gem 'guard-spork','1.5.0'
+    gem 'spork','0.9.2'
 end
 
 group :test do
     gem 'capybara','1.1.2'
+    #gem 'rb-inotify','~>0.8.8'
+    gem 'libnotify','~>0.5.9'
+    gem 'rb-inotify', '~> 0.9'
 end
 
 group :production do
-    gem 'pg','0.12.2'
 end
 
 # Gems used only for assets and not required
--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -1,34 +1,50 @@
-require 'spec_helper'
+require "spec_helper"
+
 
 describe "Static pages" do
+let(:base_title){"Ruby on Rails Tutorial Sample App"}
   describe "Home page" do
     it "should have the content 'Sample App'" do
-      visit '/static_pages/home'
-      page.should have_selector('h1',:text=>'Sample App')
+      visit "/static_pages/home"
+      page.should have_selector("h1",:text=>"Sample App")
     end
     it "should have the title Home" do
-      visit '/static_pages/home'
-      page.should have_selector('title',:text=>'Home')
+      visit "/static_pages/home"
+      page.should have_selector("title",:text=>"#{base_title} | Home")
+    end
+    it "should have the basis title" do
+      visit "/static_pages/home"
+      page.should have_selector("title",:text=>"#{base_title}")
     end
   end
   describe "Help page" do
     it "should have the content 'Help'" do
-      visit '/static_pages/help'
-      page.should have_selector('h1',:text=>'Help')
+      visit "/static_pages/help"
+      page.should have_selector("h1",:text=>"Help")
     end
     it "should have the h1 title Help" do
-      visit '/static_pages/help'
-      page.should have_selector('title',:text=>'Help')
+      visit "/static_pages/help"
+      page.should have_selector("title",:text=>"#{base_title} | Help")
     end
   end
   describe "about page" do
     it "should have the content 'About Us'" do
-      visit '/static_pages/about'
-      page.should have_selector('h1',:text=>'About Us')
+      visit "/static_pages/about"
+      page.should have_selector("h1",:text=>"About Us")
     end
     it "should have the h1 title 'About Us'" do
-      visit '/static_pages/about'
-      page.should have_selector('title',:text=>'About Us')
+      visit "/static_pages/about"
+      page.should have_selector("title",:text=>"#{base_title} | About Us")
+    end
+  end
+  describe "contact us" do
+    it "should have the content 'Contact Us'" do
+      visit "/static_pages/contact"
+      page.should have_selector("h1",:text=>"Contact Us")
+    end
+    it "should have the h1 title 'Contact Us'" do
+      visit "/static_pages/contact"
+      page.should have_selector("title",:text=>"#{base_title} | Contact Us")
     end
   end
 end
